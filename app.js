172748c28e73a446a1b0314af30f3a27d99bddef
Joshua Yannix <joshua.yannix@topbloc.com>
2020-07-27 23:37:59 -0500

    added addTodo function

--- app.js
+++ app.js
@@ -9,5 +9,24 @@ todoButton.addEventListener('click', addTodo);
 //Functions
 function addTodo(event){
   event.preventDefault()
-  const todoDiv = document.createElement('div')
+  //Todo Div and add the class of 'todo'
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+  // create li, add inner text and class. add it to our Todo div
+  const newTodo = document.createElement('li');
+  newTodo.innerText = 'hey';
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo)
+  // Create checkmark button
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add('complete-btn');
+  todoDiv.appendChild(completedButton)
+  // add trash button
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add('complete-btn');
+  todoDiv.appendChild(trashButton);
+  // append to list
+  todoList.appendChild(todoDiv);
 }
