820bf6d52f67543323796f47e90178112dc6e72d
Joshua Yannix <joshua.yannix@topbloc.com>
2020-07-29 23:22:50 -0500

    updates

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 const todoInput = document.querySelector('.todo-input');
+const filterOption = document.querySelector('.filter-todo');
 
 // Event Listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo)
 
 //Functions
 function addTodo(event){
@@ -47,5 +49,22 @@ function deleteCheck(e){
   if(item.classList[0] === 'complete-btn'){
     item.parentElement.classList.toggle('completed')
   }
+}
 
+function filterTodo(e){
+  const todos = todoList.childNodes;
+  console.log(todos)
+  todos.forEach(function(todo) {
+    switch (e.target.value) {
+      case 'all':
+        todo.style.display = 'flex';
+        break;
+      case 'completed':
+        if(todo.classList.contains('completed')){
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+    }
+  })
 }
